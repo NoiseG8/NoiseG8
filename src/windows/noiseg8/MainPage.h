ab3b1a0033d9cbe62e63a7b93dd1804b1054a9b2
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2022-03-12 19:16:39 +0100

    prog: added react-native

---
+++ src/windows/noiseg8/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::noiseg8::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::noiseg8::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
