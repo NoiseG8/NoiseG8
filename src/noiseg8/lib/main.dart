8ce6a64ca9b81a2f06961fd9f3ff5dbc52ba2bab
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2022-03-17 20:55:26 +0100

    prog: added dropdown items

--- src/noiseg8/lib/main.dart
+++ src/noiseg8/lib/main.dart
@@ -90,27 +90,76 @@ class TopMenu extends StatelessWidget {
     // onPressed: null),
   }
 
-  DropDownButton TopMenuItem() {
-    return DropDownButton(
-      // leading: Icon(FluentIcons.accept),
-      title: const Text('File'),
-      items: [
-        DropDownButtonItem(
-          title: const Text('Left'),
-          leading: const Icon(FluentIcons.align_left),
-          onTap: () => debugPrint('left'),
-        ),
-        DropDownButtonItem(
-          title: const Text('Center'),
-          leading: const Icon(FluentIcons.align_center),
-          onTap: () => debugPrint('center'),
-        ),
-        DropDownButtonItem(
-          title: const Text('Right'),
-          leading: const Icon(FluentIcons.align_right),
-          onTap: () => debugPrint('right'),
-        ),
-      ],
+  Widget TopMenuItem() {
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: Row(
+        children: [
+          DropDownButton(
+            // leading: Icon(FluentIcons.accept),
+            title: const Text('File'),
+            items: [
+              DropDownButtonItem(
+                title: const Text('Ctrl+N'),
+                leading: const Text('New Project'),
+                onTap: () => debugPrint('left'),
+              ),
+              DropDownButtonItem(
+                title: const Text('Ctrl+O'),
+                leading: const Text('Open Project'),
+                onTap: () => debugPrint('center'),
+              ),
+              DropDownButtonItem(
+                title: const Text('Right'),
+                leading: const Icon(FluentIcons.align_right),
+                onTap: () => debugPrint('right'),
+              ),
+            ],
+          ),
+          DropDownButton(
+            // leading: Icon(FluentIcons.accept),
+            title: const Text('Edit'),
+            items: [
+              DropDownButtonItem(
+                title: const Text('Ctrl+N'),
+                leading: const Text('New Project'),
+                onTap: () => debugPrint('left'),
+              ),
+              DropDownButtonItem(
+                title: const Text('Ctrl+O'),
+                leading: const Text('Open Project'),
+                onTap: () => debugPrint('center'),
+              ),
+              DropDownButtonItem(
+                title: const Text('Right'),
+                leading: const Icon(FluentIcons.align_right),
+                onTap: () => debugPrint('right'),
+              ),
+            ],
+          ),
+          DropDownButton(
+            // leading: Icon(FluentIcons.accept),
+            title: const Text('Preferences'),
+            items: [
+              DropDownButtonItem(
+                title: const Text('Ctrl+N'),
+                leading: const Text('New Project'),
+                onTap: () => debugPrint('left'),
+              ),
+              DropDownButtonItem(
+                title: const Text('Ctrl+O'),
+                leading: const Text('Open Project'),
+                onTap: () => debugPrint('center'),
+              ),
+              DropDownButtonItem(
+                title: const Text('Right'),
+                leading: const Icon(FluentIcons.align_right),
+                onTap: () => debugPrint('right'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
