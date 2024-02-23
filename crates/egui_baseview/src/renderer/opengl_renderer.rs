4e4c671c773edfaa87a3ffd073a3f5544dc81272
AZ Software <66299945+AZProductions@users.noreply.github.com>
2024-02-23 22:27:46 +0100

    chore; move deps and fix panic

---
+++ crates/egui_baseview/src/renderer/opengl_renderer.rs
@@ -0,0 +1,96 @@
+use baseview::Window;
+use egui_glow::Painter;
+use std::sync::Arc;
+
+pub struct Renderer {
+    glow_context: Arc<egui_glow::glow::Context>,
+    painter: Painter,
+}
+
+impl Renderer {
+    pub fn new(window: &Window) -> Self {
+        let context = window
+            .gl_context()
+            .expect("failed to get baseview gl context");
+        unsafe {
+            context.make_current();
+        }
+
+        let glow_context = Arc::new(unsafe {
+            egui_glow::glow::Context::from_loader_function(|s| context.get_proc_address(s))
+        });
+
+        let painter = egui_glow::Painter::new(Arc::clone(&glow_context), "", None)
+            .map_err(|error| {
+                log::error!("Error occurred in initializing painter:\n{}", error);
+            })
+            .unwrap();
+
+        unsafe {
+            context.make_not_current();
+        }
+
+        Self {
+            glow_context,
+            painter,
+        }
+    }
+
+    pub fn max_texture_side(&self) -> usize {
+        self.painter.max_texture_side()
+    }
+
+    pub fn render(
+        &mut self,
+        window: &Window,
+        bg_color: egui::Rgba,
+        canvas_width: u32,
+        canvas_height: u32,
+        pixels_per_point: f32,
+        egui_ctx: &mut egui::Context,
+        shapes: &mut Vec<egui::epaint::ClippedShape>,
+        textures_delta: &mut egui::TexturesDelta,
+    ) {
+        let shapes = std::mem::take(shapes);
+        let mut textures_delta = std::mem::take(textures_delta);
+
+        let context = window
+            .gl_context()
+            .expect("failed to get baseview gl context");
+        unsafe {
+            context.make_current();
+        }
+
+        unsafe {
+            use egui_glow::glow::HasContext as _;
+            self.glow_context
+                .clear_color(bg_color.r(), bg_color.g(), bg_color.b(), bg_color.a());
+            self.glow_context.clear(egui_glow::glow::COLOR_BUFFER_BIT);
+        }
+
+        for (id, image_delta) in textures_delta.set {
+            self.painter.set_texture(id, &image_delta);
+        }
+
+        let clipped_primitives = egui_ctx.tessellate(shapes, pixels_per_point);
+        let dimensions: [u32; 2] = [canvas_width, canvas_height];
+
+        self.painter
+            .paint_primitives(dimensions, pixels_per_point, &clipped_primitives);
+
+        for id in textures_delta.free.drain(..) {
+            self.painter.free_texture(id);
+        }
+
+        unsafe {
+            context.swap_buffers();
+            context.make_not_current();
+        }
+    }
+}
+
+impl Drop for Renderer {
+    fn drop(&mut self) {
+        self.painter.destroy()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,46 @@
 use nih_plug::prelude::*;
+use nih_plug_egui::{create_egui_editor, egui, widgets, EguiState};
 use std::sync::Arc;
 
-// This is a shortened version of the gain example with most comments removed, check out
-// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
-// started
+/// The time it takes for the peak meter to decay by 12 dB after switching to complete silence.
+const PEAK_METER_DECAY_MS: f64 = 150.0;
 
+/// This is mostly identical to the gain example, minus some fluff, and with a GUI.
 struct Noiseg8 {
     params: Arc<Noiseg8Params>,
+
+    /// Needed to normalize the peak meter's response based on the sample rate.
+    peak_meter_decay_weight: f32,
+    /// The current data for the peak meter. This is stored as an [`Arc`] so we can share it between
+    /// the GUI and the audio processing parts. If you have more state to share, then it's a good
+    /// idea to put all of that in a struct behind a single `Arc`.
+    ///
+    /// This is stored as voltage gain.
+    peak_meter: Arc<AtomicF32>,
 }
 
 #[derive(Params)]
 struct Noiseg8Params {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
+     /// The editor state, saved together with the parameter state so the custom scaling can be
+    /// restored.
+    #[persist = "editor-state"]
+    editor_state: Arc<EguiState>,
+
     #[id = "gain"]
     pub gain: FloatParam,
+
+    // TODO: Remove this parameter when we're done implementing the widgets
+    #[id = "foobar"]
+    pub some_int: IntParam,
 }
 
 impl Default for Noiseg8 {
     fn default() -> Self {
         Self {
             params: Arc::new(Noiseg8Params::default()),
+
+            peak_meter_decay_weight: 1.0,
+            peak_meter: Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
         }
     }
 }
@@ -30,29 +48,23 @@ impl Default for Noiseg8 {
 impl Default for Noiseg8Params {
     fn default() -> Self {
         Self {
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
+            editor_state: EguiState::from_size(300, 180),
+
+            // See the main gain example for more details
             gain: FloatParam::new(
                 "Gain",
                 util::db_to_gain(0.0),
                 FloatRange::Skewed {
                     min: util::db_to_gain(-30.0),
                     max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
                     factor: FloatRange::gain_skew_factor(-30.0, 30.0),
                 },
             )
-            // Because the gain parameter is stored as linear gain instead of storing the value as
-            // decibels, we need logarithmic smoothing
             .with_smoother(SmoothingStyle::Logarithmic(50.0))
             .with_unit(" dB")
-            // There are many predefined formatters we can use here. If the gain was stored as
-            // decibels instead of as a linear gain value, we could have also used the
-            // `.with_step_size(0.1)` function to get internal rounding.
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+            some_int: IntParam::new("Something", 3, IntRange::Linear { min: 0, max: 3 }),
         }
     }
 }
@@ -67,22 +79,18 @@ impl Plugin for Noiseg8 {
 
     // The first audio IO layout is used as the default. The other layouts may be selected either
     // explicitly or automatically by the host or the user depending on the plugin API/backend.
-    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
-        main_input_channels: NonZeroU32::new(2),
-        main_output_channels: NonZeroU32::new(2),
-
-        aux_input_ports: &[],
-        aux_output_ports: &[],
-
-        // Individual ports and the layout as a whole can be named here. By default these names
-        // are generated as needed. This layout will be called 'Stereo', while a layout with
-        // only one input and output channel would be called 'Mono'.
-        names: PortNames::const_default(),
-    }];
-
-
-    const MIDI_INPUT: MidiConfig = MidiConfig::None;
-    const MIDI_OUTPUT: MidiConfig = MidiConfig::None;
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[
+        AudioIOLayout {
+            main_input_channels: NonZeroU32::new(2),
+            main_output_channels: NonZeroU32::new(2),
+            ..AudioIOLayout::const_default()
+        },
+        AudioIOLayout {
+            main_input_channels: NonZeroU32::new(1),
+            main_output_channels: NonZeroU32::new(1),
+            ..AudioIOLayout::const_default()
+        },
+    ];
 
     const SAMPLE_ACCURATE_AUTOMATION: bool = true;
 
@@ -99,21 +107,82 @@ impl Plugin for Noiseg8 {
         self.params.clone()
     }
 
+    fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        let params = self.params.clone();
+        let peak_meter = self.peak_meter.clone();
+        create_egui_editor(
+            self.params.editor_state.clone(),
+            (),
+            |_, _| {},
+            move |egui_ctx, setter, _state| {
+                egui::CentralPanel::default().show(egui_ctx, |ui| {
+                    // NOTE: See `plugins/diopser/src/editor.rs` for an example using the generic UI widget
+
+                    // This is a fancy widget that can get all the information it needs to properly
+                    // display and modify the parameter from the parametr itself
+                    // It's not yet fully implemented, as the text is missing.
+                    ui.label("Some random integer");
+                    ui.add(widgets::ParamSlider::for_param(&params.some_int, setter));
+
+                    ui.label("Gain");
+                    ui.add(widgets::ParamSlider::for_param(&params.gain, setter));
+
+                    ui.label(
+                        "Also gain, but with a lame widget. Can't even render the value correctly!",
+                    );
+                    // This is a simple naieve version of a parameter slider that's not aware of how
+                    // the parameters work
+                    ui.add(
+                        egui::widgets::Slider::from_get_set(-30.0..=30.0, |new_value| {
+                            match new_value {
+                                Some(new_value_db) => {
+                                    let new_value = util::gain_to_db(new_value_db as f32);
+
+                                    setter.begin_set_parameter(&params.gain);
+                                    setter.set_parameter(&params.gain, new_value);
+                                    setter.end_set_parameter(&params.gain);
+
+                                    new_value_db
+                                }
+                                None => util::gain_to_db(params.gain.value()) as f64,
+                            }
+                        })
+                        .suffix(" dB"),
+                    );
+
+                    // TODO: Add a proper custom widget instead of reusing a progress bar
+                    let peak_meter =
+                        util::gain_to_db(peak_meter.load(std::sync::atomic::Ordering::Relaxed));
+                    let peak_meter_text = if peak_meter > util::MINUS_INFINITY_DB {
+                        format!("{peak_meter:.1} dBFS")
+                    } else {
+                        String::from("-inf dBFS")
+                    };
+
+                    let peak_meter_normalized = (peak_meter + 60.0) / 60.0;
+                    ui.allocate_space(egui::Vec2::splat(2.0));
+                    ui.add(
+                        egui::widgets::ProgressBar::new(peak_meter_normalized)
+                            .text(peak_meter_text),
+                    );
+                });
+            },
+        )
+    }
+
     fn initialize(
         &mut self,
         _audio_io_layout: &AudioIOLayout,
-        _buffer_config: &BufferConfig,
+        buffer_config: &BufferConfig,
         _context: &mut impl InitContext<Self>,
     ) -> bool {
-        // Resize buffers and perform other potentially expensive initialization operations here.
-        // The `reset()` function is always called right after this function. You can remove this
-        // function if you do not need it.
-        true
-    }
+        // After `PEAK_METER_DECAY_MS` milliseconds of pure silence, the peak meter's value should
+        // have dropped by 12 dB
+        self.peak_meter_decay_weight = 0.25f64
+            .powf((buffer_config.sample_rate as f64 * PEAK_METER_DECAY_MS / 1000.0).recip())
+            as f32;
 
-    fn reset(&mut self) {
-        // Reset buffers and envelopes here. This can be called from the audio thread and may not
-        // allocate. You can remove this function if you do not need it.
+        true
     }
 
     fn process(
@@ -123,11 +192,29 @@ impl Plugin for Noiseg8 {
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
         for channel_samples in buffer.iter_samples() {
-            // Smoothing is optionally built into the parameters themselves
-            let gain = self.params.gain.smoothed.next();
+            let mut amplitude = 0.0;
+            let num_samples = channel_samples.len();
 
+            let gain = self.params.gain.smoothed.next();
             for sample in channel_samples {
                 *sample *= gain;
+                amplitude += *sample;
+            }
+
+            // To save resources, a plugin can (and probably should!) only perform expensive
+            // calculations that are only displayed on the GUI while the GUI is open
+            if self.params.editor_state.is_open() {
+                amplitude = (amplitude / num_samples as f32).abs();
+                let current_peak_meter = self.peak_meter.load(std::sync::atomic::Ordering::Relaxed);
+                let new_peak_meter = if amplitude > current_peak_meter {
+                    amplitude
+                } else {
+                    current_peak_meter * self.peak_meter_decay_weight
+                        + amplitude * (1.0 - self.peak_meter_decay_weight)
+                };
+
+                self.peak_meter
+                    .store(new_peak_meter, std::sync::atomic::Ordering::Relaxed)
             }
         }
 
